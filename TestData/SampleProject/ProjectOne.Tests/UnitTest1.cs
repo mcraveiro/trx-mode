15c72ccfe950fe6adf0c8ba6416b409a79f8c9ff
Marco Craveiro <marco.craveiro@gmail.com>
2024-03-27 10:45:17 +0000

    test data: add sample tests projects and agile notebook

---
+++ TestData/SampleProject/ProjectOne.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProjectOne.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Fail("This test will fail");
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Fail("This test will fail");
+    }
+}
---
+++ TestData/SampleProject/ProjectThree.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ProjectThree.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
